89bb035d4586ac45ccf11c7a5091fbc3beb4ae9e
Grigory Sapogov <grisha.sapgv@mail.ru>
2024-03-04 09:34:50 +0500

    Initial Commit

---
+++ UIControlTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIControlTest
+//
+//  Created by Grigory Sapogov on 04.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
